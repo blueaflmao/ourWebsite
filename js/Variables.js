4cae2673b54c3a3498e62287c6134abe8513dd32
Blue <blueaflmao@outlook.com>
2020-08-05 11:56:51 +0300

    change imagelist to a pseudo list, take TessArt's and HungArt's elements based on avatar choice

--- js/Variables.js
+++ js/Variables.js
@@ -1,7 +1,13 @@
-var imagelist = [
+var imagelist = [];
+
+var TessArt = [
     "3M.jpg", "Botruc.jpg", "Contest3.jpg", "CycyHB.jpg", "Eye_2.jpg", "Eye.jpg",
     "Fiiiiireeee.jpg", "ForYoumyHung.jpg", "Halloween.jpg", "HMJ_DTIYS.jpg",
     "Inception.jpg", "Landscape_try.png", "lightbulb.jpeg", "Lordgris_DTIYS_3.PNG",
     "Me.jpg", "Omega.jpg", "PinkBean.jpg", "Realistic_portrait.jpg", "Shimizu_2.jpg",
     "Shimizu.png", "Whale.jpeg", "Woman_2.jpg", "Woman.jpg"
-] 
+];
+
+var HungArt = [
+    "Layout.jpg", "Ned_1.jpg"
+];
